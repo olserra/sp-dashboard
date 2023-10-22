d42cbaf8f7fd3d63b06c43fce6a5e3c4dc5b61fa
Otavio Serra <olserra@gmail.com>
2023-10-22 16:26:58 +0100

    Changes on card

--- pages/admin/cards.tsx
+++ pages/admin/cards.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   Card,
-  CardBody,
   Modal,
   ModalHeader,
   ModalBody,
@@ -10,6 +9,7 @@ import {
   Button,
   Textarea,
   Input,
+  Badge,
 } from "@roketid/windmill-react-ui";
 import PageTitle from "admin/components/Typography/PageTitle";
 import Layout from "admin/containers/Layout";
@@ -18,7 +18,7 @@ import Image from "next/image";
 function Cards() {
   const [courses, setCourses] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [selectedCourse, setSelectedCourse] = useState<any>(null);
 
   const BrufenImage =
     "https://pharmacareonline.qa/cdn/shop/products/brufen-syrup-100.png?v=1673683170";
@@ -41,10 +41,35 @@ function Cards() {
     try {
       // Make an API call to save the updated data to the database
       // Replace the 'YOUR_API_ENDPOINT' with the actual API endpoint
-      await axios.put("YOUR_API_ENDPOINT", selectedCourse);
+      const { course_id, client_id } = selectedCourse; // Destructure the course_id and client_id from selectedCourse
+      await axios.put(
+        `/update-course/${course_id}/${client_id}`,
+        selectedCourse
+      ); // Use backticks for string interpolation
       setIsModalOpen(false);
     } catch (error) {
       console.error("Error saving course data:", error);
+    }
+  };
+
+  interface Course {
+    course_id: number;
+    course_name: string;
+    course_description: string;
+    course_image: string;
+    badges: string[];
+  }
+
+  const handleDelete = async () => {
+    try {
+      const courseID = selectedCourse?.course_id; // Access course_id safely using optional chaining
+      const clientID = selectedCourse?.client_id; // Replace 'YOUR_CLIENT_ID' with the actual client ID
+      await axios.delete(`/delete-course/${courseID}/${clientID}`);
+      console.log("Course deleted");
+      // Add your code to handle the successful deletion of the course
+    } catch (error) {
+      console.error("Error deleting course:", error);
+      // Add your code to handle errors during the deletion process
     }
   };
 
@@ -55,6 +80,7 @@ function Cards() {
       course_description:
         "Learn about the uses and dosage of Brufen medication.",
       course_image: BrufenImage,
+      badges: ["Oral", "Daily", "Pharma Industry", "Certification"],
     },
     {
       course_id: 2,
@@ -62,6 +88,7 @@ function Cards() {
       course_description:
         "Understand the benefits and side effects of Benuron.",
       course_image: BrufenImage,
+      badges: ["Injection", "Weekly", "Pharma Industry", "Training"],
     },
     {
       course_id: 3,
@@ -69,6 +96,7 @@ function Cards() {
       course_description:
         "Comprehensive guide to the applications and risks of Aspirin.",
       course_image: BrufenImage,
+      badges: ["Tablet", "Monthly", "Pharma Industry", "Course Material"],
     },
     {
       course_id: 4,
@@ -76,6 +104,7 @@ function Cards() {
       course_description:
         "Master the art of prescribing and administering Paracetamol.",
       course_image: BrufenImage,
+      badges: ["Liquid", "As Needed", "Pharma Industry", "Training"],
     },
     // Add more courses as needed
   ];
@@ -101,23 +130,51 @@ function Cards() {
           <Card
             key={course.course_id}
             colored
-            className="flex flex-col justify-center items-center text-gray-700 border-teal-400 border-2 cursor-pointer"
+            className="grid grid-cols-2 gap-4 text-gray-700 border-teal-400 border-2 cursor-pointer"
             onClick={() => handleCardClick(course)}
           >
-            <CardBody>
+            <div className="p-4">
               <p className="mb-4 font-semibold">{course.course_name}</p>
               <p>{course.course_description}</p>
-            </CardBody>
-            <Image
-              src={course.course_image}
-              alt="Course image"
-              width={50}
-              height={50}
-              className="pb-4"
-            />
+              <div className="flex flex-wrap mt-4">
+                {course.badges.map((badge, index) => {
+                  const badgeTypes = [
+                    "primary",
+                    "neutral",
+                    "success",
+                    "danger",
+                    "warning",
+                  ];
+                  const badgeType = badgeTypes[index % badgeTypes.length] as
+                    | "primary"
+                    | "neutral"
+                    | "success"
+                    | "danger"
+                    | "warning";
+                  return (
+                    <Badge
+                      type={badgeType}
+                      key={badge}
+                      className="min-w-max mr-2 mb-2"
+                    >
+                      {badge}
+                    </Badge>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                src={course.course_image}
+                alt="Course image"
+                width={100}
+                height={100}
+              />
+            </div>
           </Card>
         ))}
       </div>
+
       {isModalOpen && selectedCourse && (
         <Modal isOpen={isModalOpen} onClose={closeModal}>
           <ModalHeader>
@@ -173,12 +230,37 @@ function Cards() {
                 crossOrigin={undefined}
               />
             </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Badges (Up to 5 words separated by commas)
+              </label>
+              <Input
+                value={(selectedCourse as any).badges.join(", ")}
+                onChange={(e) =>
+                  setSelectedCourse({
+                    ...(selectedCourse as any),
+                    badges: e.target.value
+                      .split(",")
+                      .map((badge) => badge.trim()),
+                  })
+                }
+                className="w-full px-3 py-2 placeholder-gray-300 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+                placeholder="Enter badges"
+                crossOrigin={undefined}
+              />
+            </div>
           </ModalBody>
           <ModalFooter>
             <Button layout="outline" onClick={handleCancel}>
               Cancel
             </Button>
             <Button onClick={handleSave}>Save</Button>
+            <div
+              className="absolute bottom-4 left-4 text-gray-500 cursor-pointer"
+              onClick={handleDelete}
+            >
+              <Trash2 size={20} />
+            </div>
           </ModalFooter>
         </Modal>
       )}
